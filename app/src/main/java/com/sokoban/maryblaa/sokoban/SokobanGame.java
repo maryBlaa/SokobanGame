9a9438ca9bc3b104693ade5aa8d4ba86b1c872f1
maryBlaa <mary.fueess@live.de>
2016-06-10 13:15:24 +0200

    Paddel moves

--- app/src/main/java/com/sokoban/maryblaa/sokoban/SokobanGame.java
+++ app/src/main/java/com/sokoban/maryblaa/sokoban/SokobanGame.java
@@ -6,6 +6,7 @@ import android.view.KeyEvent;
 import android.view.View;
 
 import com.sokoban.maryblaa.sokoban.collision.AABB;
+import com.sokoban.maryblaa.sokoban.collision.Point;
 import com.sokoban.maryblaa.sokoban.game.Game;
 import com.sokoban.maryblaa.sokoban.graphics.Camera;
 import com.sokoban.maryblaa.sokoban.graphics.CompareFunction;
@@ -17,8 +18,6 @@ import com.sokoban.maryblaa.sokoban.graphics.Texture;
 import com.sokoban.maryblaa.sokoban.input.InputEvent;
 import com.sokoban.maryblaa.sokoban.math.Matrix4x4;
 import com.sokoban.maryblaa.sokoban.math.Vector3;
-import com.sokoban.maryblaa.sokoban.collision.Point;
-
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,7 +36,7 @@ public class SokobanGame extends Game {
     private Texture texBall, texPaddle;
     private Material matBall, matPaddle;
     private Matrix4x4 worldBall;
-    private Matrix4x4[] worldRackets;
+    private Matrix4x4[] worldPaddles;
 
     private SpriteFont fontTitle, fontMenu;
     private TextBuffer textTitle;
@@ -48,6 +47,10 @@ public class SokobanGame extends Game {
     private int screenHeight;
     private int screenWidth;
     private AABB[] aabbMenu;
+    private float paddleTranslationX = 400f;
+    private static final float paddleSize = 200f;
+    private float[] paddlePositions = new float[]{0, 0};
+    private float[] paddleFingerPosition = new float[]{0, 0};
 
     public SokobanGame(View view) {
         super(view);
@@ -59,9 +62,11 @@ public class SokobanGame extends Game {
         private final String menuTitle;
 
         MenuEntry(String menuTitle) {
-                    this.menuTitle = menuTitle;
+            this.menuTitle = menuTitle;
         }
-    };
+    }
+
+    ;
 
     @Override
     public void initialize() {
@@ -69,14 +74,14 @@ public class SokobanGame extends Game {
         Matrix4x4 view = new Matrix4x4();
 
         projection = new Matrix4x4();
-        projection.setOrhtogonalProjection(-100f, 100f, -100f, 100f, 0.0f, 100.0f);
+        projection.setOrthogonalProjection(-100f, 100f, -100f, 100f, 0.0f, 100.0f);
         view = new Matrix4x4();
         hudCamera = new Camera();
         hudCamera.setProjection(projection);
         hudCamera.setView(view);
 
         projection = new Matrix4x4();
-        projection.setOrhtogonalProjection(-0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 8f);
+        projection.setOrthogonalProjection(-0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 8f);
         view = new Matrix4x4();
         view.translate(0, -1, 0);
         sceneCamera = new Camera();
@@ -89,10 +94,9 @@ public class SokobanGame extends Game {
 
         matPaddle = new Material();
 
-
-        worldRackets = new Matrix4x4[] {
-                Matrix4x4.createTranslation(-400f, 0, 0).scale(200f),
-                Matrix4x4.createTranslation(400f, 0, 0).scale(200f)
+        worldPaddles = new Matrix4x4[]{
+                Matrix4x4.createTranslation(-paddleTranslationX, 0, 0).scale(paddleSize),
+                Matrix4x4.createTranslation(paddleTranslationX, 0, 0).scale(paddleSize)
         };
 
         worldBall = new Matrix4x4();
@@ -138,7 +142,7 @@ public class SokobanGame extends Game {
 
         textTitle.setText("Sokoban");
 
-        for(MenuEntry entrie : MenuEntry.values() ) {
+        for (MenuEntry entrie : MenuEntry.values()) {
             textMenu[entrie.ordinal()].setText(entrie.menuTitle);
         }
 
@@ -152,7 +156,7 @@ public class SokobanGame extends Game {
                 Matrix4x4.createTranslation(0, 4 * dist, 0)
         };
 
-        aabbMenu = new AABB[] {
+        aabbMenu = new AABB[]{
                 new AABB(0, dist, 1000, -dist, MenuEntry.STARTGAME),
                 new AABB(0, dist * 2, 1000, -dist, MenuEntry.OPTIONS),
                 new AABB(0, dist * 3, 1000, -dist, MenuEntry.CREDITS),
@@ -196,36 +200,77 @@ public class SokobanGame extends Game {
 //                    Log.d(TAG, "ROTATION");
                     break;
                 case TOUCHSCREEN:
-                    Log.d(TAG, "" + inputEvent.getAction().toString());
+
+                    Vector3 screenTouchPosition = new Vector3(
+                            (inputEvent.getValues()[0] / (screenWidth / 2) - 1),
+                            -(inputEvent.getValues()[1] / (screenHeight / 2) - 1),
+                            0);
+
+                    Vector3 worldTouchPosition = hudCamera.unproject(screenTouchPosition, 1);
+
+
+                    Point touchPoint = new Point(worldTouchPosition.getX(), worldTouchPosition.getY());
+                    Log.d(TAG, touchPoint.toString());
+
                     switch (inputEvent.getAction()) {
                         case DOWN:
 
-                            Vector3 screenTouchPosition = new Vector3(
-                                    (inputEvent.getValues()[0] / (screenWidth / 2) - 1),
-                                    -(inputEvent.getValues()[1] / (screenHeight / 2) - 1),
-                                    0);
-
-                            Vector3 worldTouchPosition = hudCamera.unproject(screenTouchPosition, 1);
-
-                            Point touchPoint = new Point(worldTouchPosition.getX(), worldTouchPosition.getY());
-                            Log.d(TAG, touchPoint.toString());
-
-                            for (int i = 0; i < aabbMenu.length; ++i) {
-                                AABB aabb = aabbMenu[i];
-
-                                if (touchPoint.intersects(aabb)) {
-                                    MenuEntry entry = (MenuEntry) aabb.getRelatedObj();
-                                    parseMenuEntry(entry);
-                                }
-
+                            if (showMenu) {
+                                detectMenuTouch(touchPoint);
+                            } else {
+                                detectPaddleTouchDown(touchPoint);
                             }
+
+
                             break;
+                        case MOVE:
+                            if (!showMenu) {
+                                detectPaddleTouchMove(touchPoint);
+                            }
+
                     }
 
 
             }
             inputSystem.popEvent();
             inputEvent = inputSystem.peekEvent();
+
+        }
+    }
+
+    private void detectPaddleTouchDown(Point touchPoint) {
+
+
+        float x = touchPoint.getPosition().getX();
+        float y = touchPoint.getPosition().getY();
+
+        int index = x < 0 ? 0 : 1;
+        paddleFingerPosition[index] = y - paddlePositions[index];
+
+    }
+
+    private void detectPaddleTouchMove(Point touchPoint) {
+
+
+        float x = touchPoint.getPosition().getX();
+        float y = touchPoint.getPosition().getY();
+
+        int index = x < 0 ? 0 : 1;
+
+        if(Math.abs(paddleFingerPosition[index]) < paddleSize) {
+            paddlePositions[index] += y - (paddlePositions[index] + paddleFingerPosition[index]);
+            worldPaddles[index] = Matrix4x4.createTranslation((index == 0 ? -1 : 1) * paddleTranslationX, paddlePositions[index], 0).scale(paddleSize);
+        }
+
+    }
+
+
+    private void detectMenuTouch(Point touchPoint) {
+        for (AABB aabb : aabbMenu) {
+            if (touchPoint.intersects(aabb)) {
+                MenuEntry entry = (MenuEntry) aabb.getRelatedObj();
+                parseMenuEntry(entry);
+            }
 
         }
     }
@@ -267,22 +312,22 @@ public class SokobanGame extends Game {
     float speed = 0.15f;
     int direction = 1;
 
-    private void drawGame() {frame++;
+    private void drawGame() {
+        frame++;
 
         currentPosition += speed * direction;
-        if(Math.abs(currentPosition) > maxPosition) {
+        if (Math.abs(currentPosition) > maxPosition) {
             direction *= -1;
         }
 
-        // Strasse zeichnen
+        // Ball zeichnen
         worldBall.translate(speed * direction, 0, 0);
 //        worldBall.scale(0.99f);
-        System.out.println(frame);
         renderer.drawMesh(meshBall, matBall, worldBall);
 
-        // Bäume zeichnen
-        for (Matrix4x4 worldRacket : worldRackets)
-            renderer.drawMesh(meshPaddle, matPaddle, worldRacket);
+        // Paddles zeichnen
+        for (Matrix4x4 worldPaddle : worldPaddles)
+            renderer.drawMesh(meshPaddle, matPaddle, worldPaddle);
     }
 
     private void drawMenu() {
@@ -301,19 +346,19 @@ public class SokobanGame extends Game {
         Matrix4x4 projection;
 
         projection = new Matrix4x4();
-        projection.setOrhtogonalProjection(-width / 2, width / 2, -height / 2, height / 2, 0.0f, 100.0f);
+        projection.setOrthogonalProjection(-width / 2, width / 2, -height / 2, height / 2, 0.0f, 100.0f);
         hudCamera.setProjection(projection);
 
         projection = new Matrix4x4();
-        projection.setOrhtogonalProjection(-width / 2, width / 2, -height / 2, height / 2, 0.0f, 100.0f);
+        projection.setOrthogonalProjection(-width / 2, width / 2, -height / 2, height / 2, 0.0f, 100.0f);
         sceneCamera.setProjection(projection);
 
         matTitle.setIdentity();
-        matTitle.translate(-width / 2+35, height / 2-150, 0);
+        matTitle.translate(-width / 2 + 35, height / 2 - 150, 0);
     }
 
     public boolean onBackPressed() {
-        if(showMenu) {
+        if (showMenu) {
             parseMenuEntry(MenuEntry.QUIT);
         } else {
             showMenu = !showMenu;
